938c8d249f550bbeb2ff32f4d18eb9e567267a5c
Artem Zakevsky <artemijezalevsky@yandex.ru>
2024-01-06 21:35:14 +0300

    done textarea input + voice

--- src/components/modal/add-task/add-task.jsx
+++ src/components/modal/add-task/add-task.jsx
@@ -1,23 +1,57 @@
 import styles from "../add-task/add-task.module.css";
 import logoCloseBtn from '../../../assets/closeBtn.png';
-import { useContext } from 'react';
+import logoMicro from '../../../assets/micro.png';
+import { useContext, matchReply, useState, useRef, useEffect } from 'react';
 import BoardContext from '../../../services/boardContext.jsx';
 import { v4 as uuid } from 'uuid';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+
+
+
+
 
 function AddTask() {
     const { modalView, setModalView, backlog, setBacklog, text, setText } = useContext(BoardContext);
 
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    const textArea = useRef();
+
     const closeModal = () => {
         setModalView(false);
         setText('');
+        resetTranscript();
+        SpeechRecognition.stopListening()
     };
 
     const addNewTask = (e) => {
         e.preventDefault();
-        setBacklog([...backlog, { id: uuid(), task: text }]);
-        setModalView(false);
         setText('');
+        setBacklog([...backlog, { id: uuid(), task: textArea.current.value }]);
+        setModalView(false);
+        resetTranscript();
+        SpeechRecognition.stopListening()
+    };
+
+
+    const handleVoice = () => {
+        SpeechRecognition.startListening();
     }
+
+    useEffect(() => {
+        textArea.current.value = [`${text} ${transcript}`].join('').replace(/(^[\s]+|[\s]+$)/g, '');;
+    }, [transcript]);
+
+
+    const handleVoiceStop = () => {
+        SpeechRecognition.stopListening()
+    };
 
     return (
         <>
@@ -25,10 +59,18 @@ function AddTask() {
             <div className={`${styles.modal} ${modalView ? styles.show : null}`}>
                 <form className={`${styles.form}`}>
                     <h1 className={`${styles.title}`}>Enter the task text</h1>
-                    <textarea className={`${styles.textarea}`} value={text}
-                        onChange={e => setText(e.target.value)}>{text}</textarea>
+                    <textarea className={`${styles.textarea}`}
+                        onChange={e => setText(e.target.value)} ref={textArea}></textarea>
                     <img src={logoCloseBtn} alt="Close button image" className={`${styles.closeBtn}`} onClick={closeModal}></img>
-                    <span className={`${styles.buttonBlock} ${text.length >= 6 ? '' : styles.disabled}`} onClick={addNewTask}><button className={`${styles.buttonForm}`} type="submit">+</button></span>
+                    <img
+                        src={logoMicro}
+                        alt="Microphone logo"
+                        className={`${styles.micro}`}
+                        onClick={handleVoice}
+                    />
+
+                    <button onClick={handleVoiceStop}>Stop</button>
+                    <span className={`${styles.buttonBlock} ${text.toString().length >= 0 ? '' : styles.disabled}`} onClick={addNewTask}><button className={`${styles.buttonForm}`} type="submit">+</button></span>
                 </form>
             </div>
 
